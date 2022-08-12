36b4784323f5a4ce000d32501a36a5655cfd27d1
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-08-12 10:17:07 -0300

    splashscreen style

--- frontend/src/pages/SplashScreen/index.js
+++ frontend/src/pages/SplashScreen/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './index.css'
 
 export default function SplashScreen() {
   return (
-    <div>
-        <Link to="/Register">Register</Link>
+    <div className="splashscreen-container">
+      <div className="content-container my-auto mx-auto">
+      <h1 className="title mx-auto">SUAS NOTAS</h1>
+      <div className="button-container mt-10">
+        <button className="rounded-xl button log-in mx-5 px-7 py-2 w-56"><Link to="/Login">LOGIN</Link></button>
+        <button className="rounded-xl button register mx-5 px-7 py-2 w-56"><Link to="/Register">REGISTER</Link></button>
+      </div>
+      
+
+      </div>
     </div>
   )
 }
