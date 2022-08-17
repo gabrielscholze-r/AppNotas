90a57e3d4c628f133debbe2b5cc3b097aaaa0068
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-08-17 01:32:58 -0300

    estilos corrigidos

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -11,14 +11,14 @@ function Login() {
     }
 
   return (
-    <div className="Login">
-        <div className="contentdiv block">
-        <h1>LOGIN</h1>
-        <div><input type="text" placeholder='Email'  onChange={e => setEmail(e.target.value)} value={email} className="inputtext py-1 pr-2 pl-1 rounded"/>   </div>     
-        <div><input type="password" placeholder='Senha' onChange={e => setSenha(e.target.value)} value={senha} className="inputtext py-1 pr-2 pl-1 rounded"/> </div>     
-        <div><button onClick={() => entrar()} className="loginbutton px-5 py-2 rounded-xl">ENTRAR</button></div>  
-        <div><Link to='/'><button className="backbutton px-5 py-2 rounded-xl">VOLTAR</button></Link></div>
-        </div>
+    <div className="register-container ">
+      <div className="content-div-container block my-auto">
+        <h1 className="text-6xl mb-2">ENTRAR</h1>
+        <input type="text" placeholder='Email'  onChange={e => setEmail(e.target.value)} value={email} className="input-text mx-auto rounded px-2 py-2 my-2"/>
+        <input type="password" placeholder='Senha' onChange={e => setSenha(e.target.value)} value={senha} className="input-text mx-auto rounded px-2 py-2 my-2"/>
+          <div><button onClick={() => Login()} className="register-button px-5 py-2 rounded-xl mt-2">ENTRAR</button></div>
+          <div><Link to='/'><button className="back-button px-5 py-2 rounded-xl mt-2">VOLTAR</button></Link></div>  
+      </div>
     </div>
   );
 }
--- frontend/src/pages/SplashScreen/index.js
+++ frontend/src/pages/SplashScreen/index.js
@@ -6,13 +6,11 @@ export default function SplashScreen() {
   return (
     <div className="splashscreen-container">
       <div className="content-container my-auto mx-auto">
-      <h1 className="title mx-auto">SUAS NOTAS</h1>
-      <div className="button-container mt-10">
-      <Link to="/Login"><button className="rounded-xl button log-in mx-5 px-7 py-2 ">LOGIN</button></Link>
-        <Link to="/Register"><button className="rounded-xl button register mx-5 px-7 py-2">REGISTER</button></Link>
-      </div>
-      
-
+        <h1 className="title mx-auto">SUAS NOTAS</h1>
+        <div className="button-container mt-10">
+          <Link to="/Login"><button className="rounded-xl button log-in mx-5 px-7 py-2 ">LOGIN</button></Link>
+          <Link to="/Register"><button className="rounded-xl button register mx-5 px-7 py-2">REGISTER</button></Link>
+        </div>
       </div>
     </div>
   )
