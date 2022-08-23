a80a602c9ff60f79474116b0a0999a01f928fce7
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-08-23 19:59:46 -0300

    ajustes

--- frontend/src/components/Note/index.js
+++ frontend/src/components/Note/index.js
@@ -4,9 +4,9 @@ import './index.css'
 function Note({ nota }) {
     return (
         <div className="mx-auto px-5 py-5 rounded-xl nota-bloco my-5">
-            <h1>{nota.title}</h1>
-            <h2>{nota.subject}</h2>
-            <p>{nota.body}</p>
+            <h1 className='text-5xl uppercase py-1 mt-2'>{nota.title}</h1>
+            <h2 className='text-3xl py-1 mt-2'>{nota.subject}</h2>
+            <p className='text-2xl break-words py-1 mt-2'>{nota.body}</p>
         </div>
 
   );
